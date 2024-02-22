94ca8be8baed380a7cad0a9232121b2685a17b7d
Nicol <scaliariy1@gmail.com>
2024-02-22 15:42:22 +0200

    clear history

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "API_KEY"
+
+
+def get_data(place, days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data("Tokyo", 2))
